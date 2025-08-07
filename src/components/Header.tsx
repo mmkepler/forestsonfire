738939ceaefc8d1f4c449b54a1012c5e4008b123
M. Kepler <mmkepler@yahoo.com>
2025-08-07 16:49:12 -0600

    feat: basic header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="w-full flex flex-row justify-center items-center bg-amber-700 h-[70px]">Header</header>
+    <header className="w-full flex flex-row justify-center items-center bg-amber-700 h-[70px]">
+      <ul className="w-full flex flex-row justify-evenly item-center">
+        <li><Link href="/">Logo here</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/resources">Resources</Link></li>
+        <li><Link href="/prevention">Prevention</Link></li>
+        <li><Link href="/blog">Blog</Link></li>
+      </ul>
+    </header>
   )
 }
